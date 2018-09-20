c8f86cac7b7790def1317d5d878481989aa423ca
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-09-20 23:46:28 +0600

    New Folder added named Variable and integer file added
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

---
+++ Variable/src/Integer.java
@@ -0,0 +1,11 @@
+public class Integer {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        a = 10;
+        b = 10;
+        c = a + b;
+        System.out.println(c);
+    }
+}
