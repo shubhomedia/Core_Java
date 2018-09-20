b5e793ad50c846b1467950592dde6f031cd57162
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-09-21 00:43:21 +0600

    Variable mixed file added and do string related work ...
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

---
+++ Variable/src/Variable_Mixed.java
@@ -0,0 +1,19 @@
+public class Variable_Mixed {
+    public static void main(String[] args) {
+        int myNumber = 10;
+        int friendsNumber = 20;
+        int totalNumber;
+
+        totalNumber = myNumber + friendsNumber;
+
+        System.out.println(totalNumber);
+
+        String myFirstName = " Shubho";
+        String myLastName = "Owes";
+        String myFullName;
+
+        System.out.println("My full name:" + myLastName + myFirstName );
+
+
+    }
+}
