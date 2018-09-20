e421e195ff59847f1c7d87e5e8451811e8e57540
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-09-20 23:55:25 +0600

    new file double added and double static variable created.
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

---
+++ Variable/src/Double.java
@@ -0,0 +1,11 @@
+public class Double {
+    static double  a  = 10.10;
+    static double b = 20.20;
+    static double c =100.1 ;
+
+    static double d = a + b + c;
+
+    public static void main(String[] args) {
+        System.out.println(Double.d);
+    }
+}
