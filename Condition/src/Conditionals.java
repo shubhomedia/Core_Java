5ce842fb22f7e757e07bbdb1dd6ac86846f426c0
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-10-09 00:50:07 +0600

    new file conditional  added
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

---
+++ Condition/src/Conditionals.java
@@ -0,0 +1,13 @@
+public class Conditionals {
+    public static void main(String[] args) {
+        boolean morning = true;
+        boolean afternoon =false;
+        if(morning){
+            System.out.println("Good Morning ");
+        }
+        if (afternoon){
+            System.out.println("Good afternoon");
+        }
+
+    }
+}
