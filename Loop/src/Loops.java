5c70d9921db79ec1330f507ecf54b68ff2946709
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-10-09 01:12:36 +0600

    new for loop  added
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

--- Loop/src/Loops.java
+++ Loop/src/Loops.java
@@ -1,15 +1,28 @@
 public class Loops {
     public static void main(String[] args) {
-        System.out.println("1");
-        System.out.println("2");
-        System.out.println("3");
-        System.out.println("4");
-        System.out.println("5");
+       doWhileLoop();
 
-        int counter = 0;
+
+    }
+    static void whileLoop(){
+        int counter = 10;
         while (counter < 10) {
             counter = counter + 1;
             System.out.println(counter);
         }
     }
+    static void doWhileLoop(){
+        int counter = 10;
+        do{
+            counter = counter + 1;
+            System.out.println(counter);
+        } while (counter < 10);
+    }
+    static void forLoop(){
+        for (int counter = 0; counter < 10; counter = counter + 1){
+            System.out.println(counter);
+        }
+    }
+
+
 }
