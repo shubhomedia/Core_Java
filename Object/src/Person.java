63bb1f06e4af5f04519e121d269171cdb5827ff0
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-10-09 23:16:39 +0600

    Getter and Setter added
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

--- Object/src/Person.java
+++ Object/src/Person.java
@@ -1,4 +1,20 @@
 public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     String name;
     int age;
 }
