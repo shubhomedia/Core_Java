f34467592daac6bcad96d5410085da518a52fd0b
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-10-10 23:02:05 +0600

    loop written in loop section.
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

--- Loop/src/PrintOneToHundred.java
+++ Loop/src/PrintOneToHundred.java
@@ -1,2 +1,9 @@
 public class PrintOneToHundred {
+
+    public static void main(String[] args) {
+        int a;
+        for (a=1;a<=10;a++){
+            System.out.print(" " + a );
+        }
+    }
 }
