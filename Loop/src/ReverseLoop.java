fab15a44c4213f5eaab17d5923a400b6403fe69a
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-10-10 23:06:38 +0600

    Reverse Loop code added
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

--- Loop/src/ReverseLoop.java
+++ Loop/src/ReverseLoop.java
@@ -1,2 +1,8 @@
 public class ReverseLoop {
+    public static void main(String[] args) {
+        int a;
+        for (a=10; a >= 1; a--){
+            System.out.print(" " + a);
+        }
+    }
 }
