0e443846e4c3a243710a760bfd2019ce56894efd
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-10-10 22:44:08 +0600

    new file person class added on Method section . New method added.
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

---
+++ Method/src/Person.java
@@ -0,0 +1,26 @@
+public class Person {
+    public String getName() {
+        return name;
+    }
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    String name;
+    int age;
+
+    public Person(String name){
+        this.name = name;
+        System.out.println("Person Created");
+    }
+}
--- Method/src/ReferenceAndValueType.java
+++ Method/src/ReferenceAndValueType.java
@@ -4,7 +4,17 @@ public class ReferenceAndValueType {
         addOnetTo(x);
         System.out.println(x);
 
+        Person john;
+        john = new Person("John");
+        john.setAge(20);
+        celebratedBirthday(john);
+        System.out.println(john.getAge());
     }
+
+    private static void celebratedBirthday(Person person){
+        person.setAge(person.getAge()+ 1 );
+    }
+
     static void addOnetTo(int number){
         number = number + 1;
     }
