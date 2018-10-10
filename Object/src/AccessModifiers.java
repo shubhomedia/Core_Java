d95497f73b3049553e074a0a5e678e084f34dfab
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-10-10 22:49:10 +0600

    access modification on class
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

---
+++ Object/src/AccessModifiers.java
@@ -0,0 +1,20 @@
+public class AccessModifiers {
+    public static void main(String[] args) {
+
+    }
+
+    public void doSomethingPublic(){
+
+    }
+    private void doSomethingPrivate(){
+
+    }
+
+    protected void doSomethingProtected(){
+
+    }
+
+    void doSomething(){
+
+    }
+}
