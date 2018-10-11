0cf0228ddd283d82a06617e42e1e9bb0bd8c8704
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-10-11 23:49:45 +0600

    Lower type datetype
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

--- Variable/src/LowerType.java
+++ Variable/src/LowerType.java
@@ -1,2 +1,9 @@
 public class LowerType {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 10;
+
+        byte c = (byte) ( a+b );
+        System.out.println(c);
+    }
 }
