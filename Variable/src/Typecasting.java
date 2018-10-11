33f8911cbc5b8daef8c11ef33d845c3ad8c31c22
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-10-11 23:41:39 +0600

    new file typecasting variable added
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

--- Variable/src/Typecasting.java
+++ Variable/src/Typecasting.java
@@ -1,2 +1,10 @@
 public class Typecasting {
+    public static void main(String[] args) {
+        float f = 10.5f;
+        int a = (int)f;
+        System.out.println(f);
+        System.out.println(a);
+    }
+
+
 }
