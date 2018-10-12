60aa9074b3276937c805e2d2cfead52b5fc92c12
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-10-12 23:47:08 +0600

    simple math file added on variable folder
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

---
+++ Variable/src/SimpleSum.java
@@ -0,0 +1,9 @@
+public class SimpleSum {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 200;
+        int c;
+        c = a + b;
+        System.out.println("A + B " + c);
+    }
+}
