9830ccb6ef11fea1e5af679c8e659aa96f5cce41
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-11-01 23:41:40 +0600

    constant change to static and create a variable name sum. print to terminal.
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

--- Variable/src/Constants.java
+++ Variable/src/Constants.java
@@ -1,10 +1,13 @@
 public class Constants {
 
     //Example of Constant
-    final int WIDTH = 800;
-    final int HEIGHT = 600;
+    final static int WIDTH = 800;
+    final static int HEIGHT = 600;
 
     public static void main(String[] args) {
+
+        int sum = WIDTH + HEIGHT;
+        System.out.println(sum);
         System.out.println(Math.PI);
     }
 }
