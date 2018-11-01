63062ab5549fff10bdbafdc992118bee5a692a7f
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2018-11-01 23:39:27 +0600

    New file added. simple constant declared.
    Signed-off-by: MD.Owes Quruny Shubho <shubhomedia@gmail.com>

---
+++ Variable/src/Constants.java
@@ -0,0 +1,10 @@
+public class Constants {
+
+    //Example of Constant
+    final int WIDTH = 800;
+    final int HEIGHT = 600;
+
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+    }
+}
